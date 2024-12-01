b112667e8e873ea99e200d52b9feee68c77513a6
Julie Dujardin <julie@dujardin.pw>
2024-12-01 22:15:21 +0100

    deploy: fill db in backend

--- data/src/fill_db.py
+++ data/src/fill_db.py
@@ -1,9 +1,22 @@
 from .embedding import Embedder
 from .repository.connection import connect
 
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    stream=sys.stdout,
+)
+
 if __name__ == "__main__":
     embedder = Embedder()
+    logging.info("Pulling data & preparing embeddings")
     embedder.embed()
 
     with connect() as session:
+        logging.info("saving to DB")
         embedder.save_embeddings(session)
+
+    logging.info("update finished")
